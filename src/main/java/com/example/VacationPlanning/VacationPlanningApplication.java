5513b47d43f0a7222a59908c7a95d84cebad59ec
Madhura Gaikwad <madhuragaikwad@madhuras-air.home>
2023-10-01 11:56:21 -0400

    Initial commit

---
+++ src/main/java/com/example/VacationPlanning/VacationPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.example.VacationPlanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPlanningApplication.class, args);
+	}
+
+}
